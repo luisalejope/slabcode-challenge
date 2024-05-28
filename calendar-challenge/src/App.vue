0a4a2da829258b5637a06943d7cfe3ff8fa2ad11
Luis Alejandro Peña Murcia <luisalejandropena425@gmail.com>
2024-05-27 21:51:14 -0500

    first commit with setup

---
+++ calendar-challenge/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import Calendar from './components/Calendar.vue'
+</script>
+
+<template>
+  <Calendar />
+</template>
+
+<style scoped>
+body {
+  font-family: Arial, sans-serif;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  margin: 0;
+  background-color: #f0f0f0;
+}
+</style>
